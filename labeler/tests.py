3de43022d0145c10309fc539c60dd7470f16e160
Quinn Zepeda <developerquinnz@gmail.com>
2017-08-27 13:53:24 -0700

    the test works?!?

--- labeler/tests.py
+++ labeler/tests.py
@@ -19,8 +19,8 @@ class ImageModelTest(TestCase):
     def create_image(self, caption=caption):
         return Image.objects.create(caption=caption,
                                     taken_date=timezone.now() - datetime.timedelta(365.25 * 2.5),
-                                    file=open(os.path.join(MEDIA_ROOT, 'images', 'test_image.jpg'), 'rb'),
-                                    uploaded_date=timezone.now(),
+                                    file=os.path.join(MEDIA_ROOT, 'images', 'test_image.jpg'),
+                                    updated_date=timezone.now(),
                                     created_date=timezone.now())
 
     def test_image_creation(self):
